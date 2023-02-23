f6f39e7e4b244bc5716b5d8400df5d753f27404e
unknown <abdusalomov.aa03@gmail.com>
2023-02-23 15:11:34 +0500

    lesson 1

---
+++ lesson-1/js/script.js
@@ -0,0 +1,5 @@
+let name = "Ilya";
+
+console.log(`hello ${1}`); // hello 1. вставляем число
+console.log(`hello ${"name"}`); // hello name. вставляем строку
+console.log(`hello ${name}`); // hello Ilya. вставляем переменную name в которой значение Ilya
