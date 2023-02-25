bf0c07ee980d6d9479154dd573613ce2048d621d
unknown <abdusalomov.aa03@gmail.com>
2023-02-25 13:14:45 +0500

    reverse

---
+++ lesson-3/script.js
@@ -0,0 +1,92 @@
+// // hw1 📚
+// // if else ↷
+// if (name === "ECMAScript") console.log(true);
+// else console.log(false);
+
+// // Ternary operator ↷
+// name === "ECMAScript" ? console.log(true) : console.log(false);
+
+// // Switch ↷
+// switch (name) {
+//    case "ECMAScript": console.log(true); break;
+//    default: console.log(false); break;
+// }
+
+// // hw2 📚
+// // if else ↷
+// if (num > 0) {
+//    console.log("if, else " + 1);
+// } else if (num < 0) {
+//    console.log("if, else " + -1);
+// } else {
+//    console.log("if, else " + 0);
+// }
+
+// // Ternary operator ↷
+// num > 0
+//    ? console.log("ternaty " + 1)
+//    : num < 0
+//    ? console.log("ternary " + -1)
+//    : console.log("ternary " + 0);
+
+// // Switch ↷ 
+// // ----
+
+// // hw3 📚
+// // if else ↷
+// if (a + b < 4) {
+//    console.log("below");
+// } else {
+//    console.log("over");
+// }
+
+// // Ternary operator ↷
+// a + b < 4 ? console.log("below") : console.log("over");
+
+// // Switch ↷ 
+// // ----
+
+// // hw4 📚
+// // if else ↷
+// if (login == "Employee") {
+//    console.log("hello");
+// } else if (login == "Director") {
+//    console.log("Greetings");
+// } else if (login == "") {
+//    console.log("no login");
+// } else {
+//    console.log("");
+// }
+// // Ternary operator ↷
+// login == "Employee"
+//    ? console.log("hello")
+//    : login == "Director"
+//    ? console.log("Greetings")
+//    : login == ""
+//    ? console.log("no login")
+//    : console.log("");
+// // Switch ↷
+// switch (login) {
+//    case 'Employee': console.log("hello"); break;
+//    case 'Director': console.log("Greetings"); break;
+//    case '': console.log("no login"); break;
+//    default: console.log(""); break;
+// }
+
+
+
+// // 1)  Если переменная a равна нулю, то выведите 'Верно', иначе выведите 'Неверно'.
+// // Проверьте работу скрипта при a, равном 1, 0, -3.
+
+// // if else ↷
+// if (a === 0) console.log("Верно");
+// else console.log("Неверно");
+
+// // Ternary operator ↷
+// a === 0 ? console.log("true") : console.log("false");
+
+// // Switch ↷
+// switch (a) {
+//    case 0: console.log("true"); break;
+//    default: console.log("false"); break;
+// }
