0cb22bba37388ed84a3383d3a2dbc20d617c3061
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-03-09 19:25:27 +0500

    Object Objects-2

--- 6_03-03/script.js
+++ 6_03-03/script.js
@@ -53,11 +53,10 @@ let obj4 = {
    talaba: true,
 };
 if ("talaba" in obj4) {
-   obj4.kredit = true
-}
-else { 
+   obj4.kredit = true;
+} else {
    obj4.kredit = olinmadi;
-};
+}
 console.log("№12", obj4);
 
 // chiqish
@@ -79,7 +78,64 @@ let obj5 = {
    year: 1990,
 };
 if (2023 - obj5.year != obj5.age) {
-   console.log('№13 xato malumot kiritdingiz');
+   console.log("№13 xato malumot kiritdingiz");
 }
 // chiqish
 // 'xato malumot kiritdingiz'
+
+// 1
+function makeUser() {
+   return {
+      name: "john",
+      ref: this,
+   };
+}
+
+let user = makeUser();
+console.log("1 ", user.ref.name); //пустая строка. когда this призодит 1 он принимает значение глобальногоо обьекта window. В window есть indicator name: "". мы ссылаемя на этот код
+
+// 2
+let calculator = {
+   read (a, b) {
+      this.a = a;
+      this.b = b;
+   },
+   sum () {
+      return this.a + this.b
+   },
+   mul () {
+      return this.a * this.b
+   }
+};
+
+calculator.read(10, 5);
+console.log("2", calculator.sum());
+console.log("2", calculator.mul());
+
+
+// 3
+let ladder = {
+   step: 0,
+   up() {
+      this.step++;
+      return this
+   },
+   down() {
+      this.step--;
+      return this
+   },
+   showStep: function() {
+      console.log(this.step);
+      return this
+   }
+}
+
+// ladder.up()
+// ladder.up()
+// ladder.down()
+// ladder.showStep() // 1
+// ladder.down()
+// ladder.showStep() // 0
+
+ladder.up().up().down().showStep().down().showStep();
+
