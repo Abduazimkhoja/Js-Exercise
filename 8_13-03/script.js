96b25b089dca28317d8a2adae1e2361cf0cc6c71
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-03-15 01:21:00 +0500

    String

--- 8_13-03/script.js
+++ 8_13-03/script.js
@@ -1,6 +1,6 @@
 // lesson 8 String
 
-// 1 
+// 1
 console.log("1⇩");
 const check = (str1, str2) => {
    if (str1.includes(str2) && str2 != "") {
@@ -18,9 +18,7 @@ console.log("2⇩");
 const getCount = (str) => {
    let shelves = {};
    for (i of str) {
-      i in shelves
-         ? shelves[i]++
-         : shelves[i] = 1;
+      i in shelves ? shelves[i]++ : (shelves[i] = 1);
    }
    console.log(shelves);
 };
@@ -30,8 +28,8 @@ getCount("webbrain");
 console.log("3⇩");
 const trancate = (str, n = str.length, from = 0) => {
    console.log(str.slice(from, n));
-}
-trancate("webbrain academy", 5)
+};
+trancate("webbrain academy", 5);
 
 // 4
 console.log("4⇩");
@@ -39,41 +37,43 @@ const getCurrency = (str) => {
    let shelves = "";
    for (i of str) {
       if (isFinite(i)) {
-         shelves += i
+         shelves += i;
       }
       // console.log((i.match(/[a-z]/i))); ?????
    }
    console.log(shelves);
-}
-getCurrency("$1200")
+};
+getCurrency("$1200");
 
 // 5
 console.log("5⇩");
 const getSortString = (str) => {
-   let sort = {letter: 0, char: 0, num: 0, length: str.length};
+   let sort = { letter: 0, char: 0, num: 0, length: str.length };
    for (i of str) {
       let bynery = i.charCodeAt(0);
       if (isFinite(i)) {
-         sort.num++
-      // } else if ((bynery >= 65 && bynery <= 90) || (bynery >= 97 && bynery <= 122)) {
+         sort.num++;
+         // } else if ((bynery >= 65 && bynery <= 90) || (bynery >= 97 && bynery <= 122)) {
       } else if (i.match(/[a-z]/i)) {
-         sort.letter++
+         sort.letter++;
       } else {
-         sort.char++
+         sort.char++;
       }
    }
    console.log(sort);
-}
-getSortString("webbr@!n_2023be098v25= epaok'ew;l,AAAA")
+};
+getSortString("webbr@!n_2023be098v25= epaok'ew;l,AAAA");
 
-// 6 
-// console.log(6);
-// const textCapitalize = (str) => {
-//    // str = str.split(' ')
-//    str = str[0].toUpperCase() + str.slice(1)
-//    for(str.lastIndexOf(" "))
-//       let index = str.indexOf(" ") 
-//    console.log(index)
-// }
+// 6
+console.log("6⇩");
+const textCapitalize = (str) => {
+   let cap = "";
+   for (i in str) {
+      i == 0 || str[i - 1] == " "
+         ? cap += str[i].toUpperCase()
+         :cap += str[i].toLowerCase();
+   }
+   console.log(cap);
+};
 
-// textCapitalize("webbrain academy")
+textCapitalize("webbrain academy"); //Webbrain Academy
