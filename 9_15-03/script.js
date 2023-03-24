37e78df0bacc9f930b44a4198be2668021fc4108
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-03-24 20:16:58 +0500

    push,pop,shift,unshift,splice,slice,split,join

--- 9_15-03/script.js
+++ 9_15-03/script.js
@@ -1,4 +1,4 @@
-//lesson 9 array 1/3
+//lesson 9 array 1/3 length,at,indexOf,push,pop,shift,unshift,splice,slice,concat,reverse,split,join
 // 1
 console.log("1↴");
 const calcSumArr = (arr) => {
@@ -43,27 +43,7 @@ const camelize = (str) => {
 camelize("background-color"); // backgroundColor
 // camelize("list-style-image");
 
-// 5
-console.log("5↴");
-let user = [
-   {id: 1, year: 1998, engine: 1, name: "Tiko"},
-   {id: 2, year: 2005, engine: 1.2, name: "Matiz"},
-   {id: 3, year: 2010, engine: 1.6, name: "Gentra"},
-   {id: 4, year: 2010, engine: 1.5, name: "Cobalt"},
-   {id: 5, year: 2012, engine: 2, name: "Malibu"},
-   {id: 6, year: 2000, engine: 1.2, name: "Damas"},
-   {id: 7, year: 2018, engine: 2.4, name: "Tracker"},
-];
 
-console.log("< 2000", user.filter(item => item.year < 2000));
-console.log("> 2010", user.filter(item => item.year > 2010));
-console.log("Engine sort", user.sort((a,b) => a.engine - b.engine));
-console.log("Year sort", user.sort((a,b) => a.year - b.year));
-console.log("Alphabet sort", user.sort((a,b) => a.name.localeCompare(b.name)));
-console.log("Alphabet sort", user.sort((a,b) => a.name.localeCompare(b.name)));
-console.log("< 2000", user.map(item => {if (item.year < 2000) {item.status = "eski"}; return item}));
-console.log("2010 <= item <= 2000", user.map(item => {if (item.year >= 2000 && item.year <= 2010) {item.status = "orta"}; return item}));
-console.log("> 2010", user.map(item => {if (item.year > 2010) {item.status = "yangi"}; return item}));
 
 
 
